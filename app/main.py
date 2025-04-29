81ff35183349f65d02a53db5c365935cd8174e8e
Nazar <nazargorodyko@gmail.com>
2025-04-29 21:14:31 +0300

    olution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,35 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    load_dotenv()
+    api_key = os.getenv("API_KEY")
+    print(api_key)
+
+    payload = {"key": api_key, "q": "Paris"}
+
+    response = requests.get(
+        "http://api.weatherapi.com/v1/current.json",
+        params=payload,
+    )
+
+    result = response.json()
+
+    location = result.get("location")
+    current = result.get("current")
+    condition = current.get("condition")
+
+    city = location.get("name")
+    country = location.get("country")
+    current_time = location.get("localtime")
+    temperature = current.get("temp_c")
+    weather = condition.get("text")
+
+    print(f"{city}/{country} {current_time} "
+          f"Weather: {temperature} Celsius, {weather}")
 
 
 if __name__ == "__main__":
